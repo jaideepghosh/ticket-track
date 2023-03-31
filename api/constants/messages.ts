26a82a95db830a5291ba9f186e45098ccea64c16
jaideepghosh <er.jaideepghosh@gmail.com>
2023-03-31 22:29:34 +0530

    feat:Added message consts.

--- api/constants/messages.ts
+++ api/constants/messages.ts
@@ -1,5 +1,7 @@
 const MESSAGES = {
-    SOMETHING_WENT_WRONG: "Something went wrong."
+    INTERNAL_SERVER_ERROR: "Internal server error.",
+    SOMETHING_WENT_WRONG: "Something went wrong.",
+    MISSING_VALIDATION_KEYPATH: "Missing validation keypath."
 }
 
 export default MESSAGES;
