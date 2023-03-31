3e8d7cc8f0ef6986afc711f28df4abf787ca7129
jaideepghosh <er.jaideepghosh@gmail.com>
2023-04-01 00:13:32 +0530

    feat:Added msg consts.

--- api/constants/messages.ts
+++ api/constants/messages.ts
@@ -1,5 +1,7 @@
 const MESSAGES = {
     INTERNAL_SERVER_ERROR: "Internal server error.",
+    INVALID_REQUEST: "Invalid request.",
+    INVALID_STATUS: "Invalid status.",
     SOMETHING_WENT_WRONG: "Something went wrong.",
     MISSING_VALIDATION_KEYPATH: "Missing validation keypath."
 }
