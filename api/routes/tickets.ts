dfbd10c92ddcaadda850e917efdc5fb75c3eafac
jaideepghosh <er.jaideepghosh@gmail.com>
2023-03-31 18:51:59 +0530

    feat:Added patch ticket api

--- api/routes/tickets.ts
+++ api/routes/tickets.ts
@@ -23,10 +23,23 @@ router.post('/', async function(req: Request, res: Response, next: NextFunction)
     const result = await Tickets.create(payload);
     console.log("result:: ", result);
     
-    res.json(result);
+    return res.json(result);
   } catch (error) {
     console.log("error:: ", error);
-    
+    return res.status(500).send({message: MESSAGES.SOMETHING_WENT_WRONG});
+  }
+});
+
+// Update ticket
+router.patch('/:id', async function(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { id } = req.params;
+    const updatedTicket = await Tickets.findOneAndUpdate({_id: id}, req.body, {
+      new: true
+    });
+    return res.json(updatedTicket);
+  } catch (error) {
+    console.log("error:: ", error);
     return res.status(500).send({message: MESSAGES.SOMETHING_WENT_WRONG});
   }
 });
