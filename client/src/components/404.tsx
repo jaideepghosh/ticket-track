2f679fac0d4e521fba569262ab728e2a29ec075a
jaideepghosh <er.jaideepghosh@gmail.com>
2023-04-02 13:55:54 +0530

    feat:Added 404 page

---
+++ client/src/components/404.tsx
@@ -0,0 +1,15 @@
+const PageNotFound = () => {
+    return (
+	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+		<div className="max-w-md text-center">
+			<h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+				<span className="sr-only">Error</span>404
+			</h2>
+			<p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+			<a rel="noopener noreferrer" href="/" className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</a>
+		</div>
+	</div>
+    );
+}
+
+export default PageNotFound;
