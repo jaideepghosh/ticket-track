06d15d35dc19f2f5e340c90fd23292c31bf08538
jaideepghosh <er.jaideepghosh@gmail.com>
2023-04-02 14:10:32 +0530

    feat:Added routes in the app.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Tickets from './components/Tickets';
-import TicketInfo from './components/TicketInfo';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Tickets from "./components/Tickets";
+import TicketInfo from "./components/TicketInfo";
+import PageNotFound from "./components/404";
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/tickets" element={<Tickets/>}/>
-          <Route path="/ticket/:id" element={<TicketInfo/>}/>
-        </Routes>
-      </Router>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+          <div className="bg-gray-200">
+            <div className="flex h-screen w-full flex-col items-center justify-center gap-y-2">
+                <div className="w-[600px] rounded-xl py-4 px-2 items-center">
+                  <BrowserRouter>
+                    <Routes>
+                        <Route index element={<Tickets />} />
+                        <Route path="tickets/:ticketId" element={<TicketInfo />} />
+                        <Route path="*" element={<PageNotFound />} />
+                    </Routes>
+                  </BrowserRouter>
+                </div>
+            </div>
+          </div>
+        </div>
+    );
 }
 
 export default App;
